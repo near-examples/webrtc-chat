45fd0573bf561666948ff6a202e52b88ec3bd802
Evgeny Kuzyakov <ek@nearprotocol.com>
2020-03-21 09:16:19 -0700

    Updates

---
+++ precache-manifest.41013c8f0bec667183ba5af63ff2d54c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "049b15a55fb93fd22923c25ab052aeb7",
+    "url": "/webrtc-chat/index.html"
+  },
+  {
+    "revision": "0f706af7f715553b9d68",
+    "url": "/webrtc-chat/static/css/main.00a6ba7b.chunk.css"
+  },
+  {
+    "revision": "182b8331204b78936524",
+    "url": "/webrtc-chat/static/js/2.7ac1e5c3.chunk.js"
+  },
+  {
+    "revision": "a8659539666f01faf7a39edf6361ec26",
+    "url": "/webrtc-chat/static/js/2.7ac1e5c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f706af7f715553b9d68",
+    "url": "/webrtc-chat/static/js/main.8f841cf2.chunk.js"
+  },
+  {
+    "revision": "db192e89814432eff5a0",
+    "url": "/webrtc-chat/static/js/runtime-main.82c4a33d.js"
+  }
+]);
